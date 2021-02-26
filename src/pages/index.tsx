33754e05452670dc2692d95615264e92fc5dcd37
jbg <joaobenine@gmail.com>
2021-02-26 09:49:36 -0300

    get dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import { CompleteChallenges } from '../components/CompleteChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import {GetServerSideProps} from 'next';
+
 
 import Heade from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 import { ChallangeBox } from '../components/ChallengeBox';
 import { CountdownProvider } from '../contexts/CountdownContext';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -32,4 +34,23 @@ export default function Home() {
       </CountdownProvider>
     </div>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async(ctx) =>{  
+  
+ const dataUser = {
+   level: 1,
+   currentExperience: 2,
+   challengesCompleted: 2
+ } 
+  
+const {leve, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  return {
+    props:{
+     props: dataUser
+    }
+
+  }
 }
